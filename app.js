3bff8b47453bfdd90a176d93022e11e8ab982468
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-07-08 17:54:50 +0300

    feat: created a login view

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ Ext.application({
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
     //
-    mainView: 'ExtJSApp.view.main.Main'
+    // mainView: 'ExtJSApp.view.main.Main' removed
 
     //-------------------------------------------------------------------------
     // Most customizations should be made to ExtJSApp.Application. If you need to
---
+++ app/view/login/Login.js
@@ -0,0 +1,43 @@
+Ext.define('ExtJSApp.view.login.Login', {
+  extend: 'Ext.window.Window',
+  xtype: 'login',
+
+  requires: [
+    'ExtJSApp.view.login.LoginController',
+    'Ext.form.Panel'
+  ],
+
+  controller: 'login',
+  bodyPadding: 10,
+  title: 'Login Window',
+  closable: false,
+  autoShow: true,
+
+  items: {
+      xtype: 'form',
+      reference: 'form',
+      items: [{
+        xtype: 'textfield',
+        name: 'username',
+        fieldLabel: 'Логин',
+        allowBlank: false
+      }, {
+        xtype: 'textfield',
+        name: 'password',
+        inputType: 'password',
+        fieldLabel: 'Пароль',
+        allowBlank: false
+      }, {
+        xtype: 'displayfield',
+        hideEmptyLabel: false,
+        value: 'Введите пароль'
+      }],
+      buttons: [{
+        text: 'Войти',
+        formBind: true,
+        listeners: {
+          click: 'onLoginClick'
+        }
+      }]
+  }
+});
