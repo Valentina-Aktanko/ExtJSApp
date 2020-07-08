2bb5453fe2ba92309bccc082ebcd851d13f2b539
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-07-08 18:11:36 +0300

    feat: created a login controller

---
+++ app/view/login/LoginController.js
@@ -0,0 +1,15 @@
+Ext.define('ExtJSApp.view.login.LoginController', {
+  extend: 'Ext.app.ViewController',
+  alias: 'controller.login',
+
+onLoginClick: function() {
+  
+  // localStorage.setItem('ExtJSAppLoggedIn', true);
+  
+
+  this.getView().destroy();
+  Ext.create({
+    xtype: 'app-main'
+  });
+}
+});
